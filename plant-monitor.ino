e7cedd6bcf2349373a833a95113bd1916521e409
redzuan <21290781+redzuone@users.noreply.github.com>
2021-12-08 14:18:12 +0800

    Add files via upload

---
+++ plant-monitor.ino
@@ -0,0 +1,153 @@
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include <DHT.h>
+
+char auth[] = "";
+char ssid[] = "";
+char pass[] = "";
+
+#define DHTPIN 17          // What digital pin we're connected to
+#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
+DHT dht(DHTPIN, DHTTYPE);
+int dhtTimer = 0;
+
+const int soilReadPin = A0;
+const int soilStatePin = 4;
+int soilInterval = 4000L;
+int timerSoilState = 0;
+int timerSoilRead = 0;
+
+const int floatSwitchPin = 32;
+const int floatVirtualPin = V4;
+
+const int pumpPin = 27;
+int pumpState = 0;
+
+const int ldrPin = A3;
+
+BlynkTimer timer;
+
+void dhtRead()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Serial.println("temp " + String(t) +" humidity " + String(h));
+  Blynk.virtualWrite(V5, h);
+  Blynk.virtualWrite(V6, t);
+}
+
+void soilState() {
+  if(soilInterval == 4000L) {
+    Serial.println("delete timer");
+    soilInterval = 6000L;
+    timer.deleteTimer(timerSoilState);
+    timerSoilState = timer.setInterval(soilInterval, soilState);
+  }
+  digitalWrite(soilStatePin, HIGH);
+}
+
+void soilRead() {
+  int soilMoisture = analogRead(soilReadPin);
+  digitalWrite(4, LOW);
+  Serial.println("soil moisture "+ String(soilMoisture));
+  //Serial.println(soilMoisture);
+  Blynk.virtualWrite(V1, soilMoisture);
+  int timerState = timer.isEnabled(timerSoilState);
+  if(digitalRead(V0) == 1) {
+    Blynk.virtualWrite(V0, 0);
+  }
+  if(timerState == false) {
+    timer.enable(timerSoilState);
+    timer.enable(timerSoilRead);
+  }
+}
+
+BLYNK_WRITE(V0) {
+  int soilState = digitalRead(soilStatePin);
+  int soilButton = param.asInt();
+  Serial.println("hello" + String(soilButton));
+  if(soilButton == 1 && soilState == LOW) {
+    Serial.println("button pressed!");
+    
+    timer.disable(timerSoilState);
+    timer.disable(timerSoilRead);
+    digitalWrite(soilStatePin, HIGH);
+    timer.setTimeout(2000L, soilRead);
+  }
+}
+
+void floatSwitch() {
+  int floatSwitchState = digitalRead(floatSwitchPin);
+  //Serial.println("float switch: " + String(floatSwitchState));
+  Blynk.virtualWrite(floatVirtualPin, floatSwitchState);
+}
+
+void pumpOff() {
+  digitalWrite(pumpPin, HIGH);
+  pumpState = 0;
+  Serial.println("pump off");
+  Blynk.virtualWrite(V2, 0);
+  timer.enable(dhtTimer);
+}
+
+BLYNK_WRITE(V2) {
+  int button = param.asInt();
+  Serial.print("v2 button - state ");
+  Serial.println(String(button) + String(pumpState));
+  if(button == 1 && pumpState == 0) {
+    pumpState = 1;
+    Serial.println("pump on");
+    timer.disable(dhtTimer);
+    digitalWrite(pumpPin, LOW);
+    timer.setTimeout(2000L, pumpOff);
+  }/*
+  else if(timer.isEnabled(pumpTimerId == false)) {
+    digitalWrite(pumpPin, LOW);
+    Serial.println("pump off");
+  }*/
+}
+
+void getBrightness() {
+  int brightness = analogRead(ldrPin);
+  Blynk.virtualWrite(V3, brightness);
+  
+  Serial.println("brightness: " + String(brightness));
+  Serial.println("-----------------------");
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  pinMode(soilStatePin, OUTPUT);
+  pinMode(floatSwitchPin, INPUT_PULLUP);
+  pinMode(pumpPin, OUTPUT);  
+  digitalWrite(pumpPin, HIGH);
+  // Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,8,201), 8080);
+
+  dht.begin();
+  Blynk.virtualWrite(V0, 0);
+  dhtTimer = timer.setInterval(4000L, dhtRead);
+  timerSoilState = timer.setInterval(soilInterval, soilState);
+  timerSoilRead = timer.setInterval(6000L, soilRead);
+  //
+  timer.setInterval(5000L, floatSwitch);
+  timer.setInterval(5000L, getBrightness);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
